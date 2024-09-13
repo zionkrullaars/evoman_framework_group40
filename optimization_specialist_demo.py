a4ac98e74f480c66d84cba6ecf2bc6e99ce6cf23
Wouter Besse <wouterbesse@gmail.com>
2024-09-13 15:35:40 +0200

    Added bunch of explanation

--- optimization_specialist_demo.py
+++ optimization_specialist_demo.py
@@ -14,6 +14,7 @@ from wouters_test_controller import player_controller
 # imports other libs
 import time
 import numpy as np
+import random
 from math import fabs,sqrt
 import glob, os
 
@@ -93,19 +94,39 @@ def norm(x, pfit_pop):
 
 
 # evaluation
-def evaluate(x):
+def evaluate(x: list[list[tuple[np.ndarray, np.ndarray]]]) -> np.ndarray:
+    """Evaluate the fitness of a population of individuals
+
+    Args:
+        x (list[list[tuple[np.ndarray, np.ndarray]]]): Population of individuals, each individual is a list of tuples containing weights and biases for each layer
+
+    Returns:
+        np.ndarray: List of fitness values for each individual in the population of size (pop_size*1)
+    """
     return np.array(list(map(lambda y: simulation(env,y), x)))
 
 
 # tournament
-def tournament(pop):
-    c1 =  np.random.randint(0,pop.shape[0], 1)
-    c2 =  np.random.randint(0,pop.shape[0], 1)
+def tournament(pop: list[list[tuple[np.ndarray, np.ndarray]]], fit_pop: np.ndarray) -> np.ndarray:
+    """Tournament function to select the fittest of two individuals in the population
 
+    Args:
+        pop (list[list[tuple[np.ndarray, np.ndarray]]]): Population of shape (pop_size*layer_amt*2)
+        fit_pop (np.ndarray): Array of fitness values for each individual in population, of shape (pop_size*1)
+
+    Returns:
+        list(tuple(weigths, bias)): One individual in the population
+    """
+
+    # Get two random values that correspond to two individuals in the population
+    c1 =  random.randint(0,len(pop)-1)
+    c2 =  random.randint(0,len(pop)-1)
+
+    # Return the fittest of the two 
     if fit_pop[c1] > fit_pop[c2]:
-        return pop[c1][0]
+        return pop[c1]
     else:
-        return pop[c2][0]
+        return pop[c2]
 
 
 # limits
@@ -125,47 +146,81 @@ def limits(x):
 # ]
 
 # crossover
-def crossover(pop: list[list[tuple[np.ndarray, np.ndarray]]]) -> np.ndarray:
+def mutate(vals: np.ndarray, probability: float) -> np.ndarray:
+    """Mutate the values of a layer
+
+    Args:
+        vals (np.ndarray): Weights or biases of a layer
+
+    Returns:
+        np.ndarray: Mutated weights or biases
+    """
+    # TODO: Add non-linearity to the mutation
+    # TODO: Add swap mutation
+    for i in range(0,len(vals)):
+        if np.random.uniform(0 ,1)<=mutation:
+            vals[i] =   vals[i]+np.random.normal(0, 1)
+
+    return vals
+
+
+def crossover(pop: list[list[tuple[np.ndarray, np.ndarray]]], fit_pop: np.ndarray) -> np.ndarray:
     """Crossover function to generate offspring from the population
 
     Args:
         pop (list[tuple[np.ndarray, np.ndarray]]): Population formatted as a an array of models containing tuples of weights and biases.
+        fit_pop (np.ndarray): Array of fitness values for each individual in population, of shape (pop_size*1)
 
     Returns:
         _type_: _description_
     """    
-
+    # TODO: Add incest
     total_offspring = np.zeros((0,n_vars))
 
-
+    # Goes through pairs in the population and chooses two random fit individuals according to tournament
     for p in range(0,pop.shape[0], 2):
-        p1 = tournament(pop)
-        p2 = tournament(pop)
+        p1 = tournament(pop, fit_pop)
+        p2 = tournament(pop, fit_pop)
 
         n_offspring =   np.random.randint(1,3+1, 1)[0]
-        offspring =  np.zeros( (n_offspring, n_vars) )
+        offspring: list[tuple[np.ndarray, np.ndarray]] = []
 
 
         for f in range(0,n_offspring):
-            cross_prop = np.random.uniform(0,1)
-            offspring[f] = p1*cross_prop+p2*(1-cross_prop)
+            cross_prop = np.random.uniform(0,1) # Get a random ratio of influence between parents p1 and p2
+            child = []
 
-            # mutation
-            # TODO: Add non-linearity to the mutation
-            # TODO: Add swap mutation
-            for i in range(0,len(offspring[f])):
-                if np.random.uniform(0 ,1)<=mutation:
-                    offspring[f][i] =   offspring[f][i]+np.random.normal(0, 1)
+            for layer_p1, layer_p2 in zip(p1, p2):
+                # crossover
+                weights = layer_p1[1]*cross_prop+layer_p2[1]*(1-cross_prop)
+                bias = layer_p1[0]*cross_prop+layer_p2[0]*(1-cross_prop)
 
-            offspring[f] = np.array(list(map(lambda y: limits(y), offspring[f])))
+                # mutation
+                weights = mutate(weights, mutation)
+                bias = mutate(bias, mutation)
 
-            total_offspring = np.vstack((total_offspring, offspring[f]))
+                # limit between -1 and 1
+                # TODO: Do this through non-linear tanh function (see nnlayers.py for other non linear functions)
+                bias = bias.clip(-1, 1)
+                weights = weights.clip(-1, 1)
+                
+                child.append((bias, weights))
 
-    return total_offspring
+            offspring.append(child)
+
+    return offspring
 
 
-# kills the worst genomes, and replace with new best/random solutions
-def doomsday(pop,fit_pop):
+def doomsday(pop: list[tuple[np.ndarray, np.ndarray]],fit_pop:np.ndarray) -> tuple[list[tuple[np.ndarray, np.ndarray]], np.ndarray]:
+    """Kills the worst genomes, and replace with new best/random solutions
+
+    Args:
+        pop (list[tuple[np.ndarray, np.ndarray]]): Population formatted as a an array of models containing tuples of weights and biases.
+        fit_pop (np.ndarray): Array of fitness values for each individual in population, of shape (pop_size*1)
+
+    Returns:
+        _type_: _description_
+    """
 
     worst = int(npop/4)  # a quarter of the population
     order = np.argsort(fit_pop)
@@ -257,20 +312,21 @@ notimproved = 0
 
 for i in range(ini_g+1, gens):
 
-    offspring = crossover(pop)  # crossover
+    offspring = crossover(pop, fit_pop)  # crossover
     fit_offspring = evaluate(offspring)   # evaluation
-    pop = np.vstack((pop,offspring))
+    pop = pop + offspring
     fit_pop = np.append(fit_pop,fit_offspring)
 
     best = np.argmax(fit_pop) #best solution in generation
-    fit_pop[best] = float(evaluate(np.array([pop[best] ]))[0]) # repeats best eval, for stability issues
+    fit_pop[best] = float(evaluate([pop[best] ])[0]) # repeats best eval, for stability issues
     best_sol = fit_pop[best]
 
     # selection
+    # TODO: Add sigma scaling
     fit_pop_cp = fit_pop
     fit_pop_norm =  np.array(list(map(lambda y: norm(y,fit_pop_cp), fit_pop))) # avoiding negative probabilities, as fitness is ranges from negative numbers
-    probs = (fit_pop_norm)/(fit_pop_norm).sum()
-    chosen = np.random.choice(pop.shape[0], npop , p=probs, replace=False)
+    probs = (fit_pop_norm)/(fit_pop_norm).sum() # normalize fitness values to probabilities
+    chosen = np.random.choice(len(pop), npop , p=probs, replace=False)
     chosen = np.append(chosen[1:],best)
     pop = pop[chosen]
     fit_pop = fit_pop[chosen]
